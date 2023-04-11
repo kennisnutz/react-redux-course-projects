b7d8df87cfa90b754827c1767fcf824bc7f8dcf3
kennisnutz <kenneth.hotson83@gmail.com>
2023-04-11 17:30:52 +0000

    implemented json-server api

--- reading-list/src/App.js
+++ reading-list/src/App.js
@@ -1,15 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  const getAllBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
@@ -31,10 +38,19 @@ function App() {
     setBooks(updatedBooks);
   };
 
+  useEffect(() => {
+    getAllBooks();
+  }, []);
+
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookList
+        books={books}
+        onDelete={deleteBookById}
+        onEdit={editBookById}
+        fetchBooks={getAllBooks}
+      />
       <BookCreate onCreate={createBook} />
     </div>
   );
